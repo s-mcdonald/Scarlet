57d558a8f4c6680d66d651d2c5adff6fcc532b4b
SamM <s.mcdonald@outlook.com.au>
2025-06-14 07:03:28 +1000

    feat: basic HelloWorld implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
@@ -35,7 +37,7 @@ int main()
         perror("socket failed");
         return 1;
     }
-    std::cout << "ScarletServer socket created " << "...\n";
+    std::cout << "ScarletServer SUCCESS: socket created " << "...\n";
     // end: minimal socket hookup for ip4
 
     // 2. Bind to port
@@ -43,19 +45,51 @@ int main()
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY; // 0.0.0.0
     address.sin_port = htons(LOCAL_PORT);
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        return 1;
+    }
+    std::cout << "ScarletServer SUCCESS: binding to port " << LOCAL_PORT << "...\n";
+
+
     // 3. Listen
     std::cout << "ScarletServer listening on port " << LOCAL_PORT << "...\n";
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        return 1;
+    }
 
     // 4. Accept one connection
     std::cout << "ScarletServer accepting connection " << "...\n";
+    new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    if (new_socket < 0) {
+        perror("accept failed");
+        return 1;
+    }
 
     // 5. Read request
     std::cout << "ScarletServer reading request " << "...\n";
+    ssize_t bytesRead = read(new_socket, buffer, sizeof(buffer) - 1);
+    if (bytesRead > 0) {
+        buffer[bytesRead] = '\0';
+        std::cout << "Received request:\n" << buffer << "\n";
+    }
 
     // 6. Send basic HTTP response
     std::cout << "ScarletServer sending response \n";
+    const char* response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 12\r\n"
+        "\r\n"
+        "Hello world";
+    send(new_socket, response, strlen(response), 0);
+    std::cout << "Response sent\n";
+
 
     // 7. Close
     std::cout << "ScarletServer shutting down "  << "...\n";
+    close(new_socket);
+    close(server_fd);
     return 0;
 }
