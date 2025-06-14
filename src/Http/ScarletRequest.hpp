62f4ea928cbf104c21d6c2e95738adff2d36fddb
SamM <s.mcdonald@outlook.com.au>
2025-06-15 06:56:18 +1000

    feat: added enum for request type

---
+++ src/Http/ScarletRequest.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace Scarlet
+{
+    enum class ScarletRequest
+    {
+        PING = 0,
+        GET,
+        SET,
+        // HSET,
+        // HGET,
+        // LPUSH,
+        // RPUSH,
+        // LPOP,
+        // RPOP,
+        // SADD,
+        // SREM,
+        // ZADD,
+        // ZREM,
+        // DEL,
+        // EXISTS,
+        // INCR,
+        // DECR,
+    };
+}
+
