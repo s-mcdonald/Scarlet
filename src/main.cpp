71eb11b2d3fc5c99ccf117f4d33c49f1e20ec32c
SamM <s.mcdonald@outlook.com.au>
2025-06-22 06:51:27 +1000

    refactor: cleanup on entry/Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
-#include <cstring>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
 #include "Logging/ServerLog.hpp"
 #include "TcpServer.hpp"
-
-#define LOCAL_PORT 8080
-#define DEFAULT_PORT 80
 
 // https://medium.com/@sakhawy/creating-an-http-server-from-scratch-ed41ef83314b
 // https://osasazamegbe.medium.com/showing-building-an-http-server-from-scratch-in-c-2da7c0db6cb7
@@ -17,17 +8,12 @@ int main()
     Scarlet::TcpServer tcpServer;
 
     Scarlet::ServerLog::info() << "ScarletServer initializing " << "...";
-    if (tcpServer.Start() == 1)
+    if (tcpServer.Start() == -1)
     {
-        perror("socket failed");
         Scarlet::ServerLog::critical() << "ScarletServer Failed to create socket connection ";
         return 1;
     }
 
-    Scarlet::ServerLog::success() << "ScarletServer socket created " << "...";
-    Scarlet::ServerLog::success() << "ScarletServer Binding to port " << LOCAL_PORT << "...";
-
-    Scarlet::ServerLog::success() << "ScarletServer Listening on port " << LOCAL_PORT << "...";
     tcpServer.StartListen();
 
     Scarlet::ServerLog::success() << "ScarletServer shutting down "  << "...";
